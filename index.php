95fe6b7be806819c256c27c2a30de4f2c7e9a37b
Jared <jared@ayrscott.com>
2021-10-29 12:57:45 -0400

    laying out status page

--- index.php
+++ index.php
@@ -1,10 +1,67 @@
-All Systems Operational
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Ayrscott Status</title>
+</head>
 
-News
+<body>
+    <header>
+        <div id="header-left">
+            <img id="logo" src="logo.png" alt="Ayrscott, LLC">
+        </div>
+        <div id="header-right">
+            Status
+        </div>
+    </header>
+    <section id=news>
+        <h1>News</h1>
+        <section>
+            <h2>2021-10-29</h2>
+            Ayrscott Status page introduced.
+        </section>
+    </section>
+    <section id=live>
+        <h1>Live Status</h1>
+        
+        <section>
+            <h2>Data Centers</h2>
+            <h3>Georgia</h3>
+            <ul>
+                <li class=true>Atlanta, GA [Linode:a]</li>
+                <li class=true>Atlanta, GA [Linode:d]</li>
+                <li class=true>Atlanta, GA [Linode:k]</li>
+            </ul>
+            <h3>Virginia</h3>
+            <ul>
+                <li class=true>Mechanicsville, VA [Comcast]</li>
+                <li class=false>Mechanicsville, VA [T-Mobile]</li>
+                <li class=true>Manassas, VA [Verizon]</li>
+            </ul>
+            <h3>Utah</h3>
+            <ul>
+                <li class=true>Ogden, UT</li>
+            </ul>
+        </section>
+        <section>
+            <h2>Heartbeats</h2>
+            <li class=true></li>
+        </section>
+        <section>
+            <h2>PaaS Providers Status Pages</h2>
+            <ul>
+                <li>WPEngine</li>
+                <li>Linode</li>
+                <li>GitHub</li>
+                <li>Heroku</li>
+            </ul>
+        </section>
+    </section>
 
-Datacenters
+</body>
 
-Services
-
-
+</html>
