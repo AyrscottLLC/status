d4384d225cb12c89d96961c83e41fae8ee2bada9
Jared <jared@ayrscott.com>
2021-10-31 20:05:39 -0400

    predis test

--- heartbeat.php
+++ heartbeat.php
@@ -1,9 +1,22 @@
 <?php
 
-// 
+require 'vendor/autoload.php';
 
-echo $_ENV['REDIS_TLS_URL'];
+$client = null;
 
+
+if(empty($_ENV['REDIS_TLS_URL'])) {
+    
+    // localhost
+    // port: 6379
+    // auth ???
+    $client = new Predis\Client();
+} else {
+    $client = new Predis\Client(getenv('REDIS_URL') . "?ssl[verify_peer_name]=0&ssl[verify_peer]=0");
+} 
+
+// $_ENV['REDIS_TLS_URL'] = 'localhost';
+// echo $_ENV['REDIS_TLS_URL'];
 
 if(isset($_REQUEST['id']) && isset($_REQUEST['key']) && isset($_REQUEST['body'])) {
 
