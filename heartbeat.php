b1b828c8365fbf512d2c3a483d57d21206c8ea2a
Jared <jared@ayrscott.com>
2021-10-31 20:14:24 -0400

    added predis

--- heartbeat.php
+++ heartbeat.php
@@ -4,16 +4,25 @@ require 'vendor/autoload.php';
 
 $client = null;
 
-
 if(empty($_ENV['REDIS_TLS_URL'])) {
     
+    echo "Connecting to local Redis database...<br>\n";
     // localhost
     // port: 6379
     // auth ???
     $client = new Predis\Client();
+    
+
 } else {
+    echo "Connecting to Heroku Redis database...<br>\n";
     $client = new Predis\Client(getenv('REDIS_URL') . "?ssl[verify_peer_name]=0&ssl[verify_peer]=0");
 } 
+echo 'Connected<br>';
+
+$client->set('foo', 'bar');
+$value = $client->get('foo');
+
+echo "value of foo = $value\n";
 
 // $_ENV['REDIS_TLS_URL'] = 'localhost';
 // echo $_ENV['REDIS_TLS_URL'];
