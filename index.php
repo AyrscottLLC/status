53cf799000e8ff2aecefb91dfdf223ff97d6bf92
Kayla <kayla.marie.792@gmail.com>
2021-10-31 23:52:00 -0600

    modified margins

--- index.php
+++ index.php
@@ -39,7 +39,8 @@ foreach ($servers as $server) {
 }
 
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -60,77 +61,77 @@ foreach ($servers as $server) {
             Status
         </div>
     </header>
+    <section id=wrapper>
+        <section id=news>
+            <h1>News</h1>
+            <section>
+                <h2>2021-10-29</h2>
+                Ayrscott Status page introduced.
+            </section>
+        </section>
 
-    <section id=news>
-        <h1>News</h1>
-        <section>
-            <h2>2021-10-29</h2>
-            Ayrscott Status page introduced.
+        <section id=live>
+
+            <h1>Live Status</h1>
+            <section>
+                <h2>Servers</h2>
+                <ul>
+                    <?php
+                    foreach ($servers as $server) {
+                        $last_heard_from = $results[$server];
+                        $ago = solveTime($last_heard_from);
+                        $ago_text = number_format($ago, 1);
+                        $class = 'false';
+                        if ($ago < 60) $class = 'true';
+                        echo "<li class=$class>$server (~$ago_text seconds ago)</li>\n";
+                    }
+                    ?>
+                </ul>
+            </section>
+
+            <section>
+                <h2>Data Centers</h2>
+
+                <h3>Georgia</h3>
+                <ul>
+                    <li class=true>Atlanta, GA [Linode:a]</li>
+                    <li class=true>Atlanta, GA [Linode:d]</li>
+                    <li class=true>Atlanta, GA [Linode:k]</li>
+                </ul>
+
+                <h3>Virginia</h3>
+                <ul>
+                    <li class=true>Mechanicsville, VA [Comcast]</li>
+                    <li class=false>Mechanicsville, VA [T-Mobile]</li>
+                    <li class=true>Manassas, VA [Verizon]</li>
+                </ul>
+
+                <h3>Utah</h3>
+                <ul>
+                    <li class=true>Ogden, UT</li>
+                </ul>
+
+            </section>
+            <section>
+                <h2>Application Heartbeats</h2>
+                <ul>
+                    <li class=true>CEB</li>
+                    <li class=true>BigSad.me</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>PaaS Providers Status Pages</h2>
+                <ul>
+                    <li class=link>WPEngine</li>
+                    <li class=link>Linode</li>
+                    <li class=link>GitHub</li>
+                    <li class=link>Heroku</li>
+                </ul>
+            </section>
+
         </section>
     </section>
-
-    <section id=live>
-
-        <h1>Live Status</h1>
-        <section>
-            <h2>Servers</h2>
-            <ul>
-                <?php
-                foreach ($servers as $server) {
-                    $last_heard_from = $results[$server];
-                    $ago = solveTime($last_heard_from);
-                    $ago_text = number_format($ago, 1);
-                    $class = 'false';
-                    if ($ago < 60) $class = 'true';
-                    echo "<li class=$class>$server (~$ago_text seconds ago)</li>\n";
-                }
-                ?>
-            </ul>
-        </section>
-
-        <section>
-            <h2>Data Centers</h2>
-
-            <h3>Georgia</h3>
-            <ul>
-                <li class=true>Atlanta, GA [Linode:a]</li>
-                <li class=true>Atlanta, GA [Linode:d]</li>
-                <li class=true>Atlanta, GA [Linode:k]</li>
-            </ul>
-
-            <h3>Virginia</h3>
-            <ul>
-                <li class=true>Mechanicsville, VA [Comcast]</li>
-                <li class=false>Mechanicsville, VA [T-Mobile]</li>
-                <li class=true>Manassas, VA [Verizon]</li>
-            </ul>
-
-            <h3>Utah</h3>
-            <ul>
-                <li class=true>Ogden, UT</li>
-            </ul>
-
-        </section>
-        <section>
-            <h2>Application Heartbeats</h2>
-            <ul>
-                <li class=true>CEB</li>
-                <li class=true>BigSad.me</li>
-            </ul>
-        </section>
-
-        <section>
-            <h2>PaaS Providers Status Pages</h2>
-            <ul>
-                <li class=link>WPEngine</li>
-                <li class=link>Linode</li>
-                <li class=link>GitHub</li>
-                <li class=link>Heroku</li>
-            </ul>
-        </section>
-
-    </section>
-
 </body>
 
 </html>
